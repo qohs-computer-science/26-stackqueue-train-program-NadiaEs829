ce762ab3974be2d4710da5910e60a346ebcc0069
NadiaEs829 <nadia_amores@hotmail.com>
2025-01-10 18:38:00 +0000

    working - end of 1/10

--- HelloWorldProject/src/MyProgram.java
+++ HelloWorldProject/src/MyProgram.java
@@ -1,8 +1,8 @@
 /*
- * TODO: Name
- * TODO: Date
- * TODO: Class Period
- * TODO: Program Description
+ * TODO: Name; Nadia Escamilla
+ * TODO: Date: 1/10/25
+ * TODO: Class Period: 7
+ * TODO: Program Description: construct and output the status of any train that departs the station
  */
 import java.util.Scanner;
 import java.io.File;
@@ -15,17 +15,25 @@ public class MyProgram {
 	public static void main(String[] args) {
 
 		int limitTrackA = 100000, limitTrackB = 100000, limitTrackC = 100000;
-	
+		public Stack <Object> trackZero = new Stack <Object>();
+		public Stack <Object> trackOne = new Stack <Object>();
+		public Queue <Object> trackA = new LinkedList<Object>();
+		public Queue <Object> trackB = new LinkedList<Object>();
+		public Queue <Object> trackC = new LinkedList<Object>();
+		public Queue <Object> trackD = new LinkedList<Object>();
 		Scanner x = new Scanner(System.in);
 		try{
 			File f = new File("HelloWorldProject/src/data.txt");
 			x = new Scanner (f);
 			String name = x.nextLine();
 			System.out.println(name);
+			trackZero.add(name);
 		}
 		catch(Exception e)
 		{
 			System.out.println(e.getMessage());
 		}
-	}
-}
+	}//end main
+		//for()
+
+}//end class
--- HelloWorldProject/src/Train.java
+++ HelloWorldProject/src/Train.java
@@ -2,5 +2,25 @@ public class Train {
     private String name, product, origin, destination;
     private int weight, miles;
 
+    //creates a train
+    public Train(String carName, String contains, String start, String end, int w, int m){
+        name = carName;
+        product = contains;
+        origin = start;
+        destination = end;
+        weight =  w;
+        miles = m;
+    }//end constructor
 
-}
+    public String getName(){
+        return name;            //is this necessary?
+    }//end get method
+
+    public String toString(){
+        return name + product + origin + destination + weight + miles;      //add in a new line bewteen
+    }//end toString
+
+    public String getProduct(){
+        return name + " containing " + product;
+    }//end toString
+}//end class
